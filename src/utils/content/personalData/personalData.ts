43a1240ba41a7e7c86949e2f0d6484a50b14d543
RubenRaMar <rubenfr1@hotmail.com>
2023-07-25 00:52:38 +0200

    Implement the Pageheader component in the index page

--- src/utils/content/personalData/personalData.ts
+++ src/utils/content/personalData/personalData.ts
@@ -4,6 +4,11 @@ export const personalData: PersonalDataStructure = {
   name: "Rubén",
   firstSurname: "Ramírez",
   secondSurname: "Martínez",
+  tagline:
+    "I am a Creative Developer who is currently based in Barcelona, Spain.",
+  profileImage: "/assets/profileImage.webp",
+  altProfileImage: () =>
+    `${personalData.name} ${personalData.firstSurname} smiling and wearing a brown sweater`,
 };
 
 export default personalData;
---
+++ src/utils/content/roles/roles.ts
@@ -0,0 +1,18 @@
+import type { RolesStructure } from "../types";
+
+const roles: RolesStructure[] = [
+  {
+    role: "Developer",
+    icon: "code",
+  },
+  {
+    role: "Team Player",
+    icon: "microsoft-teams-logo",
+  },
+  {
+    role: "Problem-Solver",
+    icon: "bug",
+  },
+];
+
+export default roles;
